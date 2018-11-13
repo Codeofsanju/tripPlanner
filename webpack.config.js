b3d262a7c002938c59546315549ebec094484eec
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2018-11-13 16:07:12 -0500

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+        output:{
+            path: __dirname + "/public",
+            filename: "bundle.js"
+        },
+        entry: "./src/index.js",
+        mode: "development"
+}
